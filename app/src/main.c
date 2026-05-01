e2843090430f8a0005e5e6c82ae8253ed60bdfab
Ellie <elliehartung@outlook.com>
2026-05-01 16:38:58 -0400

    add example code

--- app/src/main.c
+++ app/src/main.c
@@ -1,3 +1,15 @@
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+
 int main(void) {
+    LOG_INF("Starting YuReader...");
+
+    while (1) {
+        LOG_INF("loop");
+        k_sleep(K_SECONDS(1));
+    }
+
     return 0;
 }
